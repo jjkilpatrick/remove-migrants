90da734d260eaa05f04e31d42602cc38cd26262f
John Kilpatrick <john.kilpatrick@analogfolk.com>
2015-09-18 12:22:29 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,42 @@
+(function() {
+
+    //Native tree walker from:
+    // http://stackoverflow.com/questions/2579666/getelementsbytagname-equivalent-for-textnodes
+    var getTextNodesCollection = function() {
+        var node, textNodes = [],
+            walker = document.createTreeWalker(
+            document.body,
+            NodeFilter.SHOW_TEXT, // select only text nodes
+            null,
+            false
+        );
+
+        while(node = walker.nextNode()) {
+            textNodes.push(node);
+        }
+
+        return textNodes;
+    };
+
+    var init = function() {
+        document.title = replaceInstances(document.title);
+        var textNodes = getTextNodesCollection();
+        for (var i = 0; i < textNodes.length; i++) {
+            textNodes[i].nodeValue = replaceInstances(textNodes[i].nodeValue);
+        }
+    };
+    
+    var replaceInstances = function(text) {
+        if(text.match(/migrant/gi)){
+            text = text.replace(/migrant/g, 'fellow human');
+            text = text.replace(/Fellow Human/g, 'Fellow Human');
+            text = text.replace(/migrans/g, 'fellow humans');
+            text = text.replace(/Migrant/g, 'Fellow Human');
+            text = text.replace(/Migrants/g, 'Fellow Humans');
+        }
+        return text;
+    };
+    
+    init();
+
+})();
